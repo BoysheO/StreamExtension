66b3ad4ba15edfa2768dc4fbe1caa650dd68b79f
boysheo <boysheo@outlook.com>
2021-12-13 17:52:49 +0800

    1

---
+++ BoysheO.StreamExtension/BoysheO.StreamExtension/StreamExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BoysheO.System.IO.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static async IAsyncEnumerable<ArraySegment<byte>> AsAsyncEnumerable(this Stream stream, int buffsize = 1024)
+        {
+            while (true)
+            {
+
+                var buff = ArrayPool<byte>.Shared.Rent(buffsize);
+                var count = await stream.ReadAsync(buff, 0, buffsize);
+                if (count <= 0)
+                {
+                    ArrayPool<byte>.Shared.Return(buff);
+                    yield break;
+                }
+                yield return new ArraySegment<byte>(buff, 0, count);
+            }
+        }
+    }
+}
